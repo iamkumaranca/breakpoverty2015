9386e063b2a0908c9da3640f44272f6e490f2ff3
Kumaran <tsathianathan@gmail.com>
2015-11-21 18:57:43 -0500

    tranlation class

--- inc/php/translator.class.php
+++ inc/php/translator.class.php
@@ -3,7 +3,7 @@ class Traslator_API {
 
     private $input;
     private $output;
-    //private $apiKey = 'AIzaSyC1PWVzJuKaqc2ub2nf_oti4WEQ956DyUE';
+    private $apiKey = 'AIzaSyAww6xe_uCN25DTo84zsMyXlFsLGwkopZc';
 
     function __construct( $input ) {
 		$this->input = $input;
@@ -20,8 +20,7 @@ class Traslator_API {
     }
 
     function translateInput() {
-        $apiKey = 'AIzaSyAww6xe_uCN25DTo84zsMyXlFsLGwkopZc';
-        $url = 'https://www.googleapis.com/language/translate/v2/detect?q=' . $this->input .'&key=' . $apiKey;
+        $url = 'https://www.googleapis.com/language/translate/v2/detect?q=' . $this->input .'&key=' . $this->apiKey;
 
         $handle = curl_init($url);
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);     //We want the result to be saved into variable, not printed out
