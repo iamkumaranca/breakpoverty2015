82e2df43b7150b4a0f6d5883d5f3f74fdce01a37
Kumaran <tsathianathan@gmail.com>
2015-11-21 09:45:57 -0500

    added google translate api

--- index.php
+++ index.php
@@ -5,29 +5,32 @@
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title></title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
-
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
 
+        <div id="sourceText">Hello world</div>
+            <div id="translation"></div>
+            <script>
+                function translateText(response) {
+                    document.getElementById("translation").innerHTML += "<br>" + response.data.translations[0].translatedText;
+                }
+            </script>
+            <script>
+                var newScript = document.createElement('script');
+                newScript.type = 'text/javascript';
+                var sourceText = escape(document.getElementById("sourceText").innerHTML);
+                var source = 'https://www.googleapis.com/language/translate/v2?key=AIzaSyChfy5ao_OoY9962aJOou2nA2OF5YNAEM8&source=en&target=de&callback=translateText&q=' + sourceText;
+                newScript.src = source;
+
+                // When we add this script to the head, the request is sent off.
+                document.getElementsByTagName('head')[0].appendChild(newScript);
+            </script>
+
+
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
         <script>window.jQuery || document.write('<script src="./inc/js/jquery-2.1.4.min.js"><\/script>')</script>
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='https://www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
     </body>
 </html>
