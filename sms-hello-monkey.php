c296eddb76f183929024ed1d9b1514292a623977
Kumaran <tsathianathan@gmail.com>
2015-11-21 14:11:11 -0500

    pulling string from sms

--- sms-hello-monkey.php
+++ sms-hello-monkey.php
@@ -2,15 +2,17 @@
 	header("content-type: text/xml");
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
+    require('./inc/php/Twilio.php');
+    
     $response = new Services_Twilio_Twiml;
-$body = $_REQUEST['Body'];
+    $body = $_REQUEST['Body'];
 
-if( $body == 'hello' ){
-    $response->message('Hi!');
-}else if( $body == 'bye' ){
-    $response->message('Goodbye');
-}
-print $response;
+    if( $body == 'hello' ){
+        $response->message('Hi!');
+    }else if( $body == 'bye' ){
+        $response->message('Goodbye');
+    }
+    print $response;
 ?>
 <!-- <Response>
 	<Message>Hello, Mobile Monkey</Message>
