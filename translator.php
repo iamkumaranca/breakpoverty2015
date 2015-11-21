ad835409403e3375ed0b8ce381440891043b73c9
Kumaran <tsathianathan@gmail.com>
2015-11-21 15:18:18 -0500

    pulling string from sms

--- translator.php
+++ translator.php
@@ -2,6 +2,77 @@
 header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 $body = $_REQUEST['Body'];
+
+echo '<!doctype html>
+<html class="no-js" lang="en-ca">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script>window.jQuery || document.write(\'<script src="./inc/js/jquery-2.1.4.min.js"><\/script>\')</script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <h1>Welcome to the SMS Translator</h1>
+
+        <div id="sourceText">Hello world, we are testing SMS Translation!</div>
+        <div id="translation"></div>
+
+        <script>
+        $.ajax({
+            url: \'https://www.googleapis.com/language/translate/v2/detect?q=' . $body . '&key=AIzaSyChfy5ao_OoY9962aJOou2nA2OF5YNAEM8\',
+            dataType: \'json\',
+            type: \'GET\',
+            success: function(data) {
+                translate_string(data);
+            },
+            error: function(data){
+                console.log(data);
+            }
+        });
+
+        function translate_string(detected_language) {
+            var language = detected_language.data.detections[0][0].language;
+
+            if ("en" == language) {
+                $.ajax({
+                    url: \'https://www.googleapis.com/language/translate/v2?key=AIzaSyChfy5ao_OoY9962aJOou2nA2OF5YNAEM8&q=' . $body . '&source=en&target=sw\',
+                    dataType: \'json\',
+                    type: \'GET\',
+                    success: function(data) {
+                        send_translation(data);
+                    },
+                    error: function(data){
+                        console.log(data);
+                    }
+                });
+            } else {
+                $.ajax({
+                    url: \'https://www.googleapis.com/language/translate/v2?q=' . $body . '&target=en&key=AIzaSyChfy5ao_OoY9962aJOou2nA2OF5YNAEM8\',
+                    dataType: \'json\',
+                    type: \'GET\',
+                    success: function(data) {
+                        send_translation(data);
+                    },
+                    error: function(data){
+                        console.log(data);
+                    }
+                });
+            }
+        }
+
+        function send_translation(translated_word) {
+            $translation = translated_word.data.translation[0][0].translatedText;
+        }
+        </script>
+    </body>
+</html>';
+
 ?>
 <Response>
 	<Message><? echo $body; ?> RESEND</Message>
