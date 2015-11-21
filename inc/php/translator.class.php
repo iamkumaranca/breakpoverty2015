0fb8ae22c00706bfabe47e8dffb00d6b99c162cf
Kumaran <tsathianathan@gmail.com>
2015-11-21 18:28:32 -0500

    tranlation class

--- inc/php/translator.class.php
+++ inc/php/translator.class.php
@@ -20,7 +20,8 @@ class Traslator_API {
     }
 
     function translateInput() {
-        $languageDetect = 'https://www.googleapis.com/language/translate/v2/detect?q=' . rawurlencode($this->input) . '&key=' . $this->apiKey;
+        //'https://www.googleapis.com/language/translate/v2/detect?q=hello&key=' . $this->apiKey;
+        $languageDetect = 'https://www.googleapis.com/language/translate/v2/detect?q=hello&key=' . $this->apiKey;
         $handle = curl_init($languageDetect);
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
         $response = curl_exec($handle);
