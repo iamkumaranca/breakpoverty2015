cf90a24989b02463dc554a003834dd42549995f1
Kumaran <tsathianathan@gmail.com>
2015-11-21 11:59:20 -0500

    translating string from detected language

--- translator.php
+++ translator.php
@@ -38,7 +38,7 @@
 
             if ("en" == language) {
                 $.ajax({
-                    url: 'https://www.googleapis.com/language/translate/v2?key=AIzaSyChfy5ao_OoY9962aJOou2nA2OF5YNAEM8&source=en&target=de&q=hello',
+                    url: 'https://www.googleapis.com/language/translate/v2?key=AIzaSyChfy5ao_OoY9962aJOou2nA2OF5YNAEM8&source=en&target=sw&q=hello',
                     dataType: 'json',
                     type: 'GET',
                     success: function(data) {
