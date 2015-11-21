e3e3eb05679354e81dfa8d3f8536e2bb97f6a96a
Kumaran <tsathianathan@gmail.com>
2015-11-21 11:13:56 -0500

    detecting language

--- translator.php
+++ translator.php
@@ -24,7 +24,7 @@
             dataType: 'json',
             type: 'GET',
             success: function(data) {
-                console.log(data.detections);
+                console.log(data.data.detections[0]);
             },
             error: function(data){
                 console.log(data);
