3784668fec13eff7422e8713fba1442d6859c101
Kumaran <tsathianathan@gmail.com>
2015-11-21 18:48:57 -0500

    tranlation class

--- inc/php/translator.class.php
+++ inc/php/translator.class.php
@@ -27,8 +27,9 @@ class Traslator_API {
         curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);     //We want the result to be saved into variable, not printed out
         $response = curl_exec($handle);
         curl_close($handle);
-
+        echo '<pre>';
         print_r(json_decode($response, true));
+        echo '</pre>';
         }
 
  }
