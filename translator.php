e7e8cec43046748b81f3315c8ea3d3b6e29720d3
Kumaran <tsathianathan@gmail.com>
2015-11-21 15:07:35 -0500

    pulling string from sms

--- translator.php
+++ translator.php
@@ -1,9 +1,7 @@
 <?php
-	header("content-type: text/xml");
-	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-
-    $body = $_REQUEST['Body'];
-
+header("content-type: text/xml");
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+$body = $_REQUEST['Body'];
 ?>
 <Response>
 	<Message><? echo $body; ?> RESEND</Message>
