049a5cfeef7c6ff9671bb553dd813b146da6f8ab
Kumaran <tsathianathan@gmail.com>
2015-11-21 15:10:28 -0500

    pulling string from sms

--- translator.php
+++ translator.php
@@ -7,25 +7,10 @@ $body = $_REQUEST['Body'];
 	<Message><? echo $body; ?> RESEND</Message>
 </Response>
 
-<!doctype html>
-<html class="no-js" lang="en-ca">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title></title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="./inc/js/jquery-2.1.4.min.js"><\/script>')</script>
-    </head>
-    <body>
-        <!--[if lt IE 8]>
-            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
 
-        <h1>Welcome to the SMS Translator</h1>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="./inc/js/jquery-2.1.4.min.js"><\/script>')</script>
 
-        <div id="sourceText">Hello world, we are testing SMS Translation!</div>
-        <div id="translation"></div>
 
         <script>
         $.ajax({
@@ -80,5 +65,3 @@ $body = $_REQUEST['Body'];
             $translation = translated_word.data.translation[0][0].translatedText;
         }
         </script>
-    </body>
-</html>
