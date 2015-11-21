94431aa6a8e80c4e725d3a14dfc60c4bdd08342f
Kumaran <tsathianathan@gmail.com>
2015-11-21 14:18:08 -0500

    pulling string from sms

--- sms-hello-monkey.php
+++ sms-hello-monkey.php
@@ -3,8 +3,8 @@
 	echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
     $body = $_REQUEST['Body'];
-    echo $body;
+
 ?>
 <Response>
-	<Message>Hello, Mobile Monkey</Message>
+	<Message><? echo $body; ?></Message>
 </Response>
