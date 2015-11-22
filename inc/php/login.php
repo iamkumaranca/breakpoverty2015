4ed2f2ba3d548de26424b2d9a7a7e9a5b136aa1f
Kumaran <tsathianathan@gmail.com>
2015-11-22 09:55:48 -0500

    login issue

--- inc/php/login.php
+++ inc/php/login.php
@@ -8,28 +8,20 @@ $password = hash('sha512', $_POST["password"]);
 $stmt = $dbh->prepare('SELECT * FROM `user`;');
 $stmt->execute();
 
-$user_count = count($stmt->fetch());
-
-echo '<pre>';
-print_r($stmt->fetch('username'));
-echo '</pre>';
-
-while($row = $stmt->fetch()) {
-    // if( $row['username'] == $username && $row['password'] == $password ) :
-    //     $_SESSION["logged-in"] = true;
-    //     header("Location: http://kumaransathianathan.ca/breakpoverty2015/index.php");
-    //     die();
-    // endif;
-    echo 'before: ' . $user_count;
-    echo '<br />';
-    echo $row['username'];
-    $user_count--;
-    echo '<br />';
-    echo 'after: ' . $user_count;
-    echo '<br />';
-    // if ($user_count == 0) :
-    //     header("Location: http://kumaransathianathan.ca/breakpoverty2015/login.php?error=Incorrect username or password!");
-    //     die();
-    // endif;
+foreach ($stmt->fetch() as $results)
+{
+   echo $results["username"];
 }
+
+// while($row = $stmt->fetch()) {
+//     // if( $row['username'] == $username && $row['password'] == $password ) :
+//     //     $_SESSION["logged-in"] = true;
+//     //     header("Location: http://kumaransathianathan.ca/breakpoverty2015/index.php");
+//     //     die();
+//     // endif;
+//     // if ($user_count == 0) :
+//     //     header("Location: http://kumaransathianathan.ca/breakpoverty2015/login.php?error=Incorrect username or password!");
+//     //     die();
+//     // endif;
+// }
 ?>
