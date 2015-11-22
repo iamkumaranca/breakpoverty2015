be42403031bbacc8ded0b771863775cfc1180287
Kumaran <tsathianathan@gmail.com>
2015-11-22 05:01:08 -0500

    clean up navigation

--- inc/php/navigation.php
+++ inc/php/navigation.php
@@ -9,7 +9,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.html">SMS Translation Service</a>
+            <a class="navbar-brand" href="index.php">SMS Translation Service</a>
         </div>
         <!-- /.navbar-header -->
 
@@ -54,19 +54,19 @@
                         <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
-                                <a href="flot.html">Flot Charts</a>
+                                <a href="flot.php">Flot Charts</a>
                             </li>
                             <li>
-                                <a href="morris.html">Morris.js Charts</a>
+                                <a href="morris.php">Morris.js Charts</a>
                             </li>
                         </ul>
                         <!-- /.nav-second-level -->
                     </li>
                     <li>
-                        <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
+                        <a href="tables.php"><i class="fa fa-table fa-fw"></i> Tables</a>
                     </li>
                     <li>
-                        <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
+                        <a href="forms.php"><i class="fa fa-edit fa-fw"></i> Forms</a>
                     </li>
                 </ul>
             </div>
---
+++ logout.php
@@ -0,0 +1,5 @@
+<?
+    session_unset();
+    header("Location: http://kumaransathianathan.ca/breakpoverty2015/login.php?error=Logged out successfully!");
+    die();
+?>
