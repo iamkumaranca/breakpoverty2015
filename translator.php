adff4926d9637d286b8b9e29ca6f2419d4e6629a
Kriz <k3mayo@gmail.com>
2015-11-22 03:13:31 -0500

    db

--- translator.php
+++ translator.php
@@ -24,7 +24,7 @@ $sent_to = $_REQUEST['To'];
 $status = $_REQUEST['Status'];
 $direction = $_REQUEST['Direction'];
 //$stmt->execute();
-$translation = new Traslator_API( rawurlencode(strtolower($sent_from)) );
+$translation = new Traslator_API( rawurlencode(strtolower($date_sent)) );
 
 ?>
 <Response>
