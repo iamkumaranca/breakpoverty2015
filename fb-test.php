64c53eed8240e0893b95b87e93b19038fd006c91
Kriz <k3mayo@gmail.com>
2015-11-21 23:31:38 -0500

    firebase

--- fb-test.php
+++ fb-test.php
@@ -3,22 +3,8 @@ require ('./inc/php/firebaseLib.php');
 
 const DEFAULT_URL = 'https://breakingpoverty2015.firebaseio.com/';
 const DEFAULT_TOKEN = '98qyOIK3yGRdGfwICYqfMooqf74gwFYGaxMrGUGh';
-const DEFAULT_PATH = '/firebase/example';
 
-$firebase = new \FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
-
-// --- storing an array ---
-$test = array(
-    "foo" => "bar",
-    "i_love" => "lamp",
-    "id" => 42
-);
-$dateTime = new DateTime();
-$firebase->set(DEFAULT_PATH . '/' . $dateTime->format('c'), $test);
-
-// --- storing a string ---
+$firebase = new Firebase(DEFAULT_URL, DEFAULT_TOKEN);
 $firebase->set(DEFAULT_PATH . '/name/contact001', "John Doe");
-
-// --- reading the stored string ---
 $name = $firebase->get(DEFAULT_PATH . '/name/contact001');
 ?>
