4d4ba34c3821076fa0aaebb534d7ceaeeea8e2e9
Kumaran <tsathianathan@gmail.com>
2015-11-21 19:14:35 -0500

    tranlation class

--- translator.php
+++ translator.php
@@ -1,8 +1,10 @@
 <?php
+header("content-type: text/xml");
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 require ('./inc/php/translator.class.php');
-//$body = $_REQUEST['Body'];
-$translation = new Traslator_API( "hallo" );
+$body = $_REQUEST['Body'];
+$translation = new Traslator_API( $body );
 ?>
 <Response>
 	<Message><? echo $translation->translateInput(); ?></Message>
