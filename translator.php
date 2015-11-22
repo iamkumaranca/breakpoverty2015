d8e2398595c74e97f617647bccc4861773efa7e6
Kumaran <tsathianathan@gmail.com>
2015-11-21 19:30:18 -0500

    code clean up

--- translator.php
+++ translator.php
@@ -4,7 +4,7 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 require ('./inc/php/translator.class.php');
 $body = $_REQUEST['Body'];
-$translation = new Traslator_API( $body );
+$translation = new Traslator_API( strtolower($body) );
 ?>
 <Response>
 	<Message><? echo $translation->translateInput(); ?></Message>
