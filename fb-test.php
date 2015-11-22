8ca3cd42862006d2efc9a54aba5c8ae85185dc42
Kriz <k3mayo@gmail.com>
2015-11-21 23:13:34 -0500

    firebase

--- fb-test.php
+++ fb-test.php
@@ -1,3 +1,24 @@
 <?php
-    curl -X PUT -d '{ "alanisawesome": { "name": "Alan Turing", "birthday": "June 23, 1912" } }' 'https://breakingpoverty2015.firebaseio.com/users.json';
+require_once '<path>/inc/php/firebaseLib.php';
+
+const DEFAULT_URL = 'https://kidsplace.firebaseio.com/';
+const DEFAULT_TOKEN = 'MqL0c8tKCtheLSYcygYNtGhU8Z2hULOFs9OKPdEp';
+const DEFAULT_PATH = '/firebase/example';
+
+$firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
+
+// --- storing an array ---
+$test = array(
+    "foo" => "bar",
+    "i_love" => "lamp",
+    "id" => 42
+);
+$dateTime = new DateTime();
+$firebase->set(DEFAULT_PATH . '/' . $dateTime->format('c'), $test);
+
+// --- storing a string ---
+$firebase->set(DEFAULT_PATH . '/name/contact001', "John Doe");
+
+// --- reading the stored string ---
+$name = $firebase->get(DEFAULT_PATH . '/name/contact001');
 ?>
