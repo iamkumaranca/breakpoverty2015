34f0e3f4fe791d0a25f104e42cb2fcb4f1f944a4
Kumaran <tsathianathan@gmail.com>
2015-11-22 03:48:04 -0500

    connecting login page with database

--- inc/php/login.php
+++ inc/php/login.php
@@ -5,17 +5,13 @@ session_start();
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-echo hash("sha512", '<br/eak>');
-
 $stmt = $dbh->prepare('SELECT `id`, `username`, `password` FROM `user`;');
 $stmt->execute();
 
 while($row = $stmt->fetch()) {
 	// print_r($row)."<br/>"; // recursively print out object.
-    echo $row['username'] . '<br />' . $username . '<br /><br />';
-    echo $row['password']. '<br />' . $password . '<br /><br />';
 
-    if( $row['username'] == $username && password_verify($password, $password['password'])) {
+    if( $row['username'] == $username && hash('sha512', $password);) {
         echo 'Access Granted';
     } else {
         echo 'Access Denied';
