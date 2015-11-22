f864a8b449b045012bedc9dd5436cfe877e8c254
Kumaran <tsathianathan@gmail.com>
2015-11-22 10:00:29 -0500

    login issue

--- inc/php/login.php
+++ inc/php/login.php
@@ -8,20 +8,16 @@ $password = hash('sha512', $_POST["password"]);
 $stmt = $dbh->prepare('SELECT * FROM `user`;');
 $stmt->execute();
 
-foreach ($stmt->fetch() as $results)
-{
-   echo $results["username"];
+while($row = $stmt->fetch()) {
+    if( $row['username'] == $username && $row['password'] == $password ) :
+        $_SESSION["logged-in"] = true;
+        header("Location: http://kumaransathianathan.ca/breakpoverty2015/index.php");
+        die();
+    endif;
 }
 
-// while($row = $stmt->fetch()) {
-//     // if( $row['username'] == $username && $row['password'] == $password ) :
-//     //     $_SESSION["logged-in"] = true;
-//     //     header("Location: http://kumaransathianathan.ca/breakpoverty2015/index.php");
-//     //     die();
-//     // endif;
-//     // if ($user_count == 0) :
-//     //     header("Location: http://kumaransathianathan.ca/breakpoverty2015/login.php?error=Incorrect username or password!");
-//     //     die();
-//     // endif;
-// }
+if ($row['username'] != $username && $row['password'] != $password) :
+    header("Location: http://kumaransathianathan.ca/breakpoverty2015/login.php?error=Incorrect username or password!");
+    die();
+endif;
 ?>
