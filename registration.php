efe737d46d0d457fc01267096f3095dc9ef46e5c
Kumaran <tsathianathan@gmail.com>
2015-11-22 08:29:51 -0500

    added ability to add translated string into db

--- registration.php
+++ registration.php
@@ -21,17 +21,25 @@ if($_SESSION["logged-in"]) :
                         <div class="panel-body">
                             <div class="row">
                                 <form role="form" action="./inc/php/register.php" method="post">
+                                    <?php $error = $_GET["error"]; ?>
+                                    <?php if($error != '') : ?>
+                                    <div class="alert alert-danger" role="alert">
+                                        <span class="fa fa-exclamation-circle" aria-hidden="true"></span>
+                                        <span class="sr-only">Error:</span>
+                                        <?php echo $error; ?>
+                                    </div>
+                                    <?php endif; ?>
                                     <div class="col-lg-12">
                                         <div class="col-lg-6">
                                             <div class="form-group">
                                                 <label>First Name</label>
-                                                <input class="form-control" type="text" name="first_name" placeholder="First Name">
+                                                <input class="form-control" type="text" name="first_name" placeholder="First Name" required>
                                             </div>
                                         </div>
                                         <div class="col-lg-6">
                                             <div class="form-group">
                                                 <label>Last Name</label>
-                                                <input class="form-control"  type="text" name="last_name" placeholder="Last Name">
+                                                <input class="form-control"  type="text" name="last_name" placeholder="Last Name" required>
                                             </div>
                                         </div>
                                     </div>
@@ -39,13 +47,13 @@ if($_SESSION["logged-in"]) :
                                         <div class="col-lg-6">
                                             <div class="form-group">
                                                 <label>Email Address</label>
-                                                <input class="form-control" type="email" name="email" placeholder="Email Address">
+                                                <input class="form-control" type="email" name="email" placeholder="Email Address" required>
                                             </div>
                                         </div>
                                         <div class="col-lg-6">
                                             <div class="form-group">
                                                 <label>Mobile Phone Number</label>
-                                                <input class="form-control" type="tel" name="mobile_phone" placeholder="Mobile Phone Number">
+                                                <input class="form-control" type="tel" name="mobile_phone" placeholder="Mobile Phone Number" required>
                                             </div>
                                         </div>
                                     </div>
@@ -53,7 +61,7 @@ if($_SESSION["logged-in"]) :
                                         <div class="col-lg-6">
                                             <div class="form-group">
                                                 <label>Username</label>
-                                                <input class="form-control" type="text" name="username" placeholder="Username">
+                                                <input class="form-control" type="text" name="username" placeholder="Username" required>
                                             </div>
                                         </div>
                                         <div class="col-lg-6">
@@ -75,13 +83,13 @@ if($_SESSION["logged-in"]) :
                                         <div class="col-lg-6">
                                             <div class="form-group">
                                                 <label>Password</label>
-                                                <input class="form-control" type="password" name="password" placeholder="Password">
+                                                <input class="form-control" type="password" name="password" placeholder="Password" required>
                                             </div>
                                         </div>
                                         <div class="col-lg-6">
                                             <div class="form-group">
                                                 <label>Confirm Password</label>
-                                                <input class="form-control" type="password" name="confirm_password" placeholder="Confirm Password">
+                                                <input class="form-control" type="password" name="confirm_password" placeholder="Confirm Password" required>
                                             </div>
                                         </div>
                                     </div><div class="col-lg-12">
