236b5487d410724a8c583ccf2eb47ca07d8b5949
Kumaran <tsathianathan@gmail.com>
2015-11-22 09:07:17 -0500

    login issue

--- inc/php/login.php
+++ inc/php/login.php
@@ -11,17 +11,11 @@ $stmt->execute();
 while($row = $stmt->fetch()) {
     if( $row['username'] == $username && $row['password'] == $password ) {
         $_SESSION["logged-in"] = true;
-        //header("Location: http://kumaransathianathan.ca/breakpoverty2015/index.php");
-        //die();
+        header("Location: http://kumaransathianathan.ca/breakpoverty2015/index.php");
+        die();
     } else {
         //header("Location: http://kumaransathianathan.ca/breakpoverty2015/login.php?error=Incorrect username or password!");
         //die();
-
-        echo 'db password: ' . $row['password'] . '<br />';
-        echo 'user password: ' . $password . '<br />';
-        echo 'db username: ' . $row['username']. '<br />';
-        echo 'user username: ' . $username . '<br /><br /><br /><br /><br /><br />';
-
     }
 }
 ?>
