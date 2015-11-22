d9fc38330c808148be7627899b431a711c5b8a1e
Kriz <k3mayo@gmail.com>
2015-11-21 23:02:10 -0500

    firebase

--- fb-test.php
+++ fb-test.php
@@ -1,3 +1,3 @@
 <?php
-    curl -X PUT -d '{ "alanisawesome": { "name": "Alan Turing", "birthday": "June 23, 1912" } }' 'https://breakingpoverty2015.firebaseio.com/users.json'
+    curl -X PUT -d '{ "alanisawesome": { "name": "Alan Turing", "birthday": "June 23, 1912" } }' 'https://breakingpoverty2015.firebaseio.com/users.json';
 ?>
