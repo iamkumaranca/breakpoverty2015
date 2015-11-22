002ecf737be6ba2e2162f21291f4622a1907a511
Kumaran <tsathianathan@gmail.com>
2015-11-22 08:56:50 -0500

    login issue

--- inc/php/login.php
+++ inc/php/login.php
@@ -2,14 +2,14 @@
 require_once('./connect_db.php');
 session_start();
 
-$username = $_POST["username"];
+$username = hash('sha512', $_POST["username"]);
 $password = $_POST["password"];
 
 $stmt = $dbh->prepare('SELECT `username`, `password` FROM `user`;');
 $stmt->execute();
 
 while($row = $stmt->fetch()) {
-    if( $row['username'] == $username && $row['password'] == hash('sha512', $password) ) {
+    if( $row['username'] == $username && $row['password'] == $password ) {
         $_SESSION["logged-in"] = true;
         header("Location: http://kumaransathianathan.ca/breakpoverty2015/index.php");
         die();
