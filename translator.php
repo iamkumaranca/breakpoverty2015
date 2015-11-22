6fcbaf26d81f86632a4278ca0fa9eb936299a087
Kriz <k3mayo@gmail.com>
2015-11-22 02:59:55 -0500

    db

--- translator.php
+++ translator.php
@@ -6,13 +6,13 @@ include_once('./inc/php/connect_db.php');
 require ('./inc/php/translator.class.php');
 
 
-$stmt = $dbh->prepare("INSERT INTO sms (id, date_sent, sent_from, sent_to, message, status, direction) VALUES ('', :date_sent, :sent_from, :sent_to, :message, :status, :direction)");
+/*$stmt = $dbh->prepare("INSERT INTO sms (id, date_sent, sent_from, sent_to, message, status, direction) VALUES ('', :date_sent, :sent_from, :sent_to, :message, :status, :direction)");
 $stmt->bindParam(':date_sent', $date_sent);
 $stmt->bindParam(':sent_from', $sent_from);
 $stmt->bindParam(':sent_to', $sent_to);
 $stmt->bindParam(':message', $body);
 $stmt->bindParam(':status', $status);
-$stmt->bindParam(':direction', $direction);
+$stmt->bindParam(':direction', $direction);*/
 
 $body = $_REQUEST['Body'];
 $date_sent = $_REQUEST['Date_Sent'];
@@ -23,7 +23,7 @@ $sent_from = $_REQUEST['From'];
 $sent_to = $_REQUEST['To'];
 $status = $_REQUEST['Status'];
 $direction = $_REQUEST['Direction'];
-$stmt->execute();
+//$stmt->execute();
 $translation = new Traslator_API( rawurlencode(strtolower($body)) );
 echo $sent_from . '<br />';
 ?>
