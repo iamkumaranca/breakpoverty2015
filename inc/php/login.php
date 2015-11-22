fe482e39debe2f6f20481ef1a3ed4bc0c1c7421c
Kumaran <tsathianathan@gmail.com>
2015-11-22 02:53:41 -0500

    connecting login page with database

--- inc/php/login.php
+++ inc/php/login.php
@@ -5,14 +5,14 @@ session_start();
 $email = $_POST["email"];
 $password = $_POST["password"];
 
-	$stmt = $dbh->prepare('SELECT `id`, `username`, `password` FROM `user`;');
-	$stmt->execute();
+$stmt = $dbh->prepare('SELECT `id`, `username`, `password` FROM `user`;');
+$stmt->execute();
 
 	echo $email;
 	echo $password;
 
 	while($row = $stmt->fetch()) {
-		print_r($row)."<br/>"; // recursively print out object.
+		//print_r($row)."<br/>"; // recursively print out object.
 		if($email == $row['email']){
 			echo("WELCOME TO OUR HIDDEN PAGE!!! You are now loggged in!");
 			$_SESSION["logged-in"] = true;
