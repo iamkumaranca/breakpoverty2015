b852942409d7ad401ad3c53c856dc51aa3be7753
Kriz <k3mayo@gmail.com>
2015-11-22 02:24:17 -0500

    remove firebase plus insert to db

--- translator.php
+++ translator.php
@@ -2,8 +2,24 @@
 header("content-type: text/xml");
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
+include_once('./inc/php/connect_db.php');
 require ('./inc/php/translator.class.php');
+
+$stmt = $dbh->prepare("INSERT INTO SMS (id, date_sent, sent_from, sent_to, message, status, direction) VALUES ('', :date_sent, :sent_from, :sent_to, :message, :status, :direction)");
+$stmt->bindParam(':date_sent', $date_sent);
+$stmt->bindParam(':sent_from', $sent_from);
+$stmt->bindParam(':sent_to', $sent_to);
+$stmt->bindParam(':message', $body);
+$stmt->bindParam(':status', $status);
+$stmt->bindParam(':direction', $direction);
+
 $body = $_REQUEST['Body'];
+$date_sent = $_REQUEST['Date_Sent'];
+$sent_from = $_REQUEST['From'];
+$sent_to = $_REQUEST['To'];
+$status = $_REQUEST['Status'];
+$direction = $_REQUEST['Direction'];
+$stmt->execute();
 $translation = new Traslator_API( rawurlencode(strtolower($body)) );
 ?>
 <Response>
