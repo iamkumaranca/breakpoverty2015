90958246d91c096ad858b0f07cfbb68762935113
Kumaran <tsathianathan@gmail.com>
2015-11-22 03:03:31 -0500

    connecting login page with database

--- inc/php/login.php
+++ inc/php/login.php
@@ -2,26 +2,19 @@
 require_once('./connect_db.php');
 session_start();
 
-$email = $_POST["email"];
-$password = $_POST["password"];
+$username = $_POST["username"];
+$password = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 $stmt = $dbh->prepare('SELECT `id`, `username`, `password` FROM `user`;');
 $stmt->execute();
 
-	echo $email;
-	echo password_hash($password, PASSWORD_DEFAULT);
-
-	// while($row = $stmt->fetch()) {
-	// 	//print_r($row)."<br/>"; // recursively print out object.
-	// 	if($email == $row['email']){
-	// 		echo("WELCOME TO OUR HIDDEN PAGE!!! You are now loggged in!");
-	// 		//$_SESSION["logged-in"] = true;
-	// 	}else{
-	// 		echo("Access Denied");
-	// 	}
-	// }
-
-
-
-
+while($row = $stmt->fetch()) {
+	//print_r($row)."<br/>"; // recursively print out object.
+	if($username == $row['username']){
+		echo("WELCOME TO OUR HIDDEN PAGE!!! You are now loggged in!");
+		//$_SESSION["logged-in"] = true;
+	}else{
+		echo("Access Denied");
+	}
+}
 ?>
--- login.php
+++ login.php
@@ -11,7 +11,7 @@
                         <form role="form" action="./inc/php/login.php" method="post">
                             <fieldset>
                                 <div class="form-group">
-                                    <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+                                    <input class="form-control" placeholder="Username" name="username" type="text" autofocus>
                                 </div>
                                 <div class="form-group">
                                     <input class="form-control" placeholder="Password" name="password" type="password" value="">
