4129c2dc4da9a97d65a6dea1453c5bb3622e61c2
Kumaran <tsathianathan@gmail.com>
2015-11-22 02:45:01 -0500

    connecting login page with database

---
+++ inc/php/footer.php
@@ -0,0 +1,21 @@
+
+    <!-- jQuery -->
+    <script src="./inc/js/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="./inc/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="./inc/js/metisMenu.min.js"></script>
+
+    <!-- Morris Charts JavaScript -->
+    <script src="./inc/js/raphael-min.js"></script>
+    <script src="./inc/js/morris.min.js"></script>
+    <script src="./inc/js/morris-data.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="./inc/js/sb-admin-2.js"></script>
+
+</body>
+
+</html>
---
+++ inc/php/header.php
@@ -0,0 +1,43 @@
+<?php include_once('./inc/php/connect_db.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SMS Translation Service</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="./inc/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="./inc/css/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="./inc/css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="./inc/css/sb-admin-2.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="./inc/css/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="./inc/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
